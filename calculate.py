cd8fb55f43bd65bb2185f89d367c932c245a949c
silvimica <mayagoloburda@gmail.com>
2025-07-23 10:16:16 +0400

    Auto-commit

--- calculate.py
+++ calculate.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
-from calendar import isleap
-
-# judge the leap year
-def judge_leap_year(year):
-    if isleap(year):
-        return True
-    else:
-        return False
-
-
-# returns the number of days in each month
-def month_days(month, leap_year):
-    if month in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
-    elif month in [4, 6, 9, 11]:
-        return 30
-    elif month == 2 and leap_year:
-        return 29
-    elif month == 2 and (not leap_year):
-        return 28
+from date_utils import judge_leap_year, month_days
 
 
 name = input("input your name: ")
---
+++ test_calculate.py
@@ -0,0 +1,6 @@
+def test_is_true():
+    assert True
+
+
+def test_do_nothing():
+    pass
